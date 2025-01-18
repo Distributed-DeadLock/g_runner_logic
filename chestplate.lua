4667c839fc78f61a0788e3e949f71d4e410d23e5
Distributed-DeadLock <32169417+Distributed-DeadLock@users.noreply.github.com>
2025-01-18 06:11:23 +0100

    Update chestplate.lua

--- chestplate.lua
+++ chestplate.lua
@@ -3,7 +3,7 @@
 	armor:register_armor(":g_runner_logic:chestplate_black", {
 		description = "Black Chestplate",
 		inventory_image = "g_runner_logic_inv_chestplate_black.png",
-		groups = {armor_torso=1, armor_heal=12, armor_use=300, armor_fire=1},
+		groups = {armor_torso=1, armor_heal=12, armor_use=50, armor_fire=1},
 		armor_groups = {fleshy=20},
 		damage_groups = {cracky=2, snappy=1, choppy=1, level=3},
 	})
